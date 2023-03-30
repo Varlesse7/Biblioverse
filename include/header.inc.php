37b2dcb5fab26d2f83a76ed113efb429767aaa6a
Pierre FRANCK-PAPUCHON <pierrefranck78@hotmail.com>
2023-03-30 10:16:24 +0200

    Bouton Filtre

---
+++ include/header.inc.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title><?= $title ?></title>
+    <meta charset="utf-8"/>
+    <meta name="author" content="Franck-Papuchon"/>
+    <meta name="description" content="<?= $description ?>"/>
+    <meta name="keywords" content="Php, Franck-Papuchon Pierre, Page"/>
+    <meta name="Language" content="fr"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="date" content="15/02/2023"/>
+    <meta name="place" content="CYU Tech"/>
+    <link href="style_jour.css" rel="stylesheet"/>
+    <link rel="icon" href="./images/book.ico" type="image/icon"/>
+</head>
+<body>
+    <header>
+        <p>Profil</p>
+        <form class="filtre" method="get" action="index.php">
+            <button>Filtre</button>
+        </form>
+
+        <form class="barre_de_recherche" method="get" action="index.php">
+
+            <label>
+                <input type="search" name="q" placeholder="Recherche"/>
+            </label>
+        </form>
+        <img src="./images/petit_Icon.png">
+        
+    </header>
+
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$title = "Biblioverse";
+$description = "Vous pouvez rentrer dans le monde des livres";
+$currentPage = "index";
+
+require("include/header.inc.php")
+
+?>
+<main>
+    <h1>Site en Attendant le projet</h1>
+    <p>Site temporaire en attendant les designs pour le vrai site.</p>
+
+</main>
+
+<?php require("./include/footer.inc.php"); ?>
+</html>
+
