37b44ff4d15b2ae0993ef8882f28d0c3f87838c1
Pierre FRANCK-PAPUCHON <pierrefranck78@hotmail.com>
2023-04-03 22:01:47 +0200

    Css Premier Partie
    Css pas fini

--- include/header.inc.php
+++ include/header.inc.php
@@ -3,6 +3,7 @@
 <head>
     <title><?= $title ?></title>
     <meta charset="utf-8"/>
+
     <meta name="author" content="Franck-Papuchon"/>
     <meta name="description" content="<?= $description ?>"/>
     <meta name="keywords" content="Php, Franck-Papuchon Pierre, Page"/>
@@ -14,19 +15,24 @@
     <link rel="icon" href="./images/book.ico" type="image/icon"/>
 </head>
 <body>
-    <header>
-        <p>Profil</p>
+<header>
+    <p>Profil</p>
+    <div class="seach-bar">
         <form class="filtre" method="get" action="index.php">
             <button>Filtre</button>
         </form>
 
         <form class="barre_de_recherche" method="get" action="index.php">
-
             <label>
                 <input type="search" name="q" placeholder="Recherche"/>
             </label>
         </form>
+    </div>
+    <div class="icon">
+        <p class="title">Biblioverse</p>
         <img src="./images/petit_Icon.png">
-        
-    </header>
+
+
+    </div>
+</header>
 
--- index.php
+++ index.php
@@ -7,10 +7,40 @@ $currentPage = "index";
 require("include/header.inc.php")
 
 ?>
-<main>
-    <h1>Site en Attendant le projet</h1>
-    <p>Site temporaire en attendant les designs pour le vrai site.</p>
+<main class="black_background">
+    <article class="black_background">
+        <h1>Notre séléction de livre </h1>
+        <div class="container">
+            <div class="book-container">
+                <img src="images/L'Etranger.jpg">
+                <div>
+                    <span>L'Etranger</span>
+                    <span>Albert Camus</span>
+                    <span><strong>Avis le plus aimé :</strong>
 
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut nulla nec dolor placerat tempus a
+                            vitae nisi. In laoreet at lorem eget iaculis. Proin fringilla elit nec sapien laoreet, a pretium sem
+                            elementum. Quisque vitae congue odio. Vivamus congue massa </span>
+                </div>
+            </div>
+            <div class="book-container">
+                <img src="images/L'orient-Express.jpg">
+                <div>
+                    <span>L'orient-Express</span>
+                    <span>Agatha Christie</span>
+                    <span><strong>Avis le plus aimé :</strong>
+
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut nulla nec dolor placerat tempus a
+                            vitae nisi. In laoreet at lorem eget iaculis. Proin fringilla elit nec sapien laoreet, a pretium sem
+                            elementum. Quisque vitae congue odio. Vivamus congue massa </span>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </article>
 </main>
 
 <?php require("./include/footer.inc.php"); ?>
