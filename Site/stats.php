f9927ff9976347052e90389ffb30551865a1a16b
MrDreamer93p <64521225+MrDreamer93p@users.noreply.github.com>
2023-05-07 17:28:21 +0200

    fix bouton retour

--- Site/stats.php
+++ Site/stats.php
@@ -9,23 +9,7 @@ require("include/function.php");
 ?>
 <main>
 	<section class="orange_background">
-	<?php
-		$style_actuel = '';
-		if (isset($_GET['style'])) {
-			$style_actuel = 'style_' . $_GET['style'] . '.css';
-		} 
-		elseif (isset($_COOKIE['style'])) {
-			$style_actuel = 'style_' . $_COOKIE['style'] . '.css';
-		} 
-		else {
-			$style_actuel = 'style_nuit.css';
-		}
-		if ($style_actuel == 'style_jour.css') {
-			echo '<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line.svg" width="100" height="100" alt=""></a>';
-		} elseif ($style_actuel == 'style_nuit.css') {
-			echo '<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line-nuit.svg" width="100" height="100" alt=""></a>';
-		}
-	?>
+	<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line.svg" width="100" height="100" alt=""></a>
     <h1>Statistiques des visites</h1>
     
 	<?php
