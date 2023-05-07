90fe71637c99da26b55101fa9c21d60b768ed176
MrDreamer93p <64521225+MrDreamer93p@users.noreply.github.com>
2023-05-07 15:56:36 +0200

    Fix url

--- Site/include/header.inc.php
+++ Site/include/header.inc.php
@@ -56,72 +56,25 @@
                 } ?>">
             </label>
             <label for="genre"></label>
-            <select name="genre" id="genre">
-                <option value="" <?php if ($genre == '') {
-                    echo 'selected';
-                } ?>>Tous les genres
-                </option>
-                <option value="art" <?php if ($genre == 'art') {
-                    echo 'selected';
-                } ?>>Art
-                </option>
-                <option value="biography" <?php if ($genre == 'biography') {
-                    echo 'selected';
-                } ?>>Biographie
-                </option>
-                <option value="comics" <?php if ($genre == 'comics') {
-                    echo 'selected';
-                } ?>>BD
-                </option>
-                <option value="computers" <?php if ($genre == 'computers') {
-                    echo 'selected';
-                } ?>>Informatique
-                </option>
-                <option value="cooking" <?php if ($genre == 'cooking') {
-                    echo 'selected';
-                } ?>>Cuisine
-                </option>
-                <option value="education" <?php if ($genre == 'education') {
-                    echo 'selected';
-                } ?>>Éducation
-                </option>
-                <option value="fiction" <?php if ($genre == 'fiction') {
-                    echo 'selected';
-                } ?>>Fiction
-                </option>
-                <option value="history" <?php if ($genre == 'history') {
-                    echo 'selected';
-                } ?>>Histoire
-                </option>
-                <option value="medical" <?php if ($genre == 'medical') {
-                    echo 'selected';
-                } ?>>Médecine
-                </option>
-                <option value="music" <?php if ($genre == 'music') {
-                    echo 'selected';
-                } ?>>Musique
-                </option>
-                <option value="nature" <?php if ($genre == 'nature') {
-                    echo 'selected';
-                } ?>>Nature
-                </option>
-                <option value="poetry" <?php if ($genre == 'poetry') {
-                    echo 'selected';
-                } ?>>Poésie
-                </option>
-                <option value="religion" <?php if ($genre == 'religion') {
-                    echo 'selected';
-                } ?>>Religion
-                </option>
-                <option value="science" <?php if ($genre == 'science') {
-                    echo 'selected';
-                } ?>>Science
-                </option>
-                <option value="travel" <?php if ($genre == 'travel') {
-                    echo 'selected';
-                } ?>>Voyages
-                </option>
-            </select>
+			<select name="genre" id="genre">
+				<option value="all">Tous les genres</option>
+				<option value="art" <?php if ($genre == 'art') { echo 'selected'; } ?>>Art</option>
+				<option value="biography" <?php if ($genre == 'biography') { echo 'selected'; } ?>>Biographie</option>
+				<option value="comics" <?php if ($genre == 'comics') { echo 'selected'; } ?>>BD</option>
+				<option value="computers" <?php if ($genre == 'computers') { echo 'selected'; } ?>>Informatique</option>
+				<option value="cooking" <?php if ($genre == 'cooking') { echo 'selected'; } ?>>Cuisine</option>
+				<option value="education" <?php if ($genre == 'education') { echo 'selected'; } ?>>Éducation</option>
+				<option value="fiction" <?php if ($genre == 'fiction') { echo 'selected'; } ?>>Fiction</option>
+				<option value="history" <?php if ($genre == 'history') { echo 'selected'; } ?>>Histoire</option>
+				<option value="medical" <?php if ($genre == 'medical') { echo 'selected'; } ?>>Médecine</option>
+				<option value="music" <?php if ($genre == 'music') { echo 'selected'; } ?>>Musique</option>
+				<option value="nature" <?php if ($genre == 'nature') { echo 'selected'; } ?>>Nature</option>
+				<option value="poetry" <?php if ($genre == 'poetry') { echo 'selected'; } ?>>Poésie</option>
+				<option value="religion" <?php if ($genre == 'religion') { echo 'selected'; } ?>>Religion</option>
+				<option value="science" <?php if ($genre == 'science') { echo 'selected'; } ?>>Science</option>
+				<option value="travel" <?php if ($genre == 'travel') { echo 'selected'; } ?>>Voyages</option>
+			</select>
+
             <button class="submit" type="submit">Rechercher</button>
         </form>
 
