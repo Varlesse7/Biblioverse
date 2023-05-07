26f524bf5fffd4a440e50251ef1e2093545cf9cd
MrDreamer93p <64521225+MrDreamer93p@users.noreply.github.com>
2023-05-07 13:54:28 +0200

    Fix Alt

--- Site/recherche.php
+++ Site/recherche.php
@@ -20,9 +20,9 @@ require("include/function.php");
             $style_actuel = 'style_nuit.css';
         }
         if ($style_actuel == 'style_jour.css') {
-            echo '<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line.svg" width="100" height="100"></a>';
+            echo '<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line.svg" width="100" height="100" alt=""></a>';
         } elseif ($style_actuel == 'style_nuit.css') {
-            echo '<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line-nuit.svg" width="100" height="100"></a>';
+            echo '<a href="#" onclick="history.back()"><img src="images/arrow-go-back-line-nuit.svg" width="100" height="100" alt=""></a>';
         }
         ?>
 
